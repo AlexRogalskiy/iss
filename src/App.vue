12171759302dc3e2c520eea1013cdf4fe8bd32c1
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2020-12-14 14:59:25 +0300

    Initial repo commit

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="app">
+    <header class="app-head container grid-md">
+      <h1><a href="https://en.wikipedia.org/wiki/International_Space_Station" title="Link to Wikipedia Page" target="_blank">International Space Station</a></h1>
+      <p>The International Space Station is an amazing experiment, this page shows data feeds from the International Space Station.</p>
+    </header>
+
+    <Map :position="position" />
+    <Feed />
+
+  </div>
+</template>
+
+<style>
+  html,
+  body {
+    height: 100%;
+  }
+
+  body {
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    background-color: #f3f3f3;
+    color: #555;
+    font-size: 14px;
+    font-family: Helvetica, Verdana, sans-serif;
+    font-weight: 300;
+    margin: 0px;
+  }
+
+  a,
+  a.active,
+  a:active,
+  a:focus,
+  a:visited,
+  a:hover {
+    color: #545e98;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Anton', sans-serif;
+    font-weight: 400;
+  }
+
+  .app-head {
+    padding: 60px 0 10px;
+  }
+
+  header {
+    text-align: center;
+  }
+
+  hr {
+    border: 0;
+    height: 1px;
+    background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.30), rgba(0, 0, 0, 0));
+    margin-bottom: 40px;
+    margin-top: 40px;
+  }
+
+  #app {
+    width: 100vw;
+    height: 100%;
+  }
+</style>
+
+<script>
+import Map from './components/Map.vue'
+import Feed from './components/LiveFeed.vue'
+import L from 'leaflet'
+import { setInterval } from 'timers';
+
+export default {
+  name: 'app',
+  components: {
+    Map,
+    Feed
+  },
+  data () {
+    return {
+      position: L.latLng(0, 0)
+    }
+  },
+  methods: {
+    getPOS () {
+      setInterval(() => {
+        fetch(`http://api.open-notify.org/iss-now.json`)
+          .then((response) => {
+            return response.json()
+          })
+          .then((json) => {
+            if (json.message === "success") this.position = L.latLng(json.iss_position.latitude, json.iss_position.longitude)
+          })
+      }, 2200)
+    }
+  },
+  mounted: function () {
+    this.getPOS()
+  }
+}
+</script>
+
+
---
+++ src/components/Map.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <l-map
+      :zoom="zoom"
+      :center="position"
+      :options="mapOptions"
+      style="height:85vh;"
+    >
+      <l-tile-layer :url="url" :attribution="attribution" />
+      <l-marker :lat-lng="position" :icon="icon" />
+    </l-map>
+  </div>
+</template>
+
+<style>
+  @import "../../node_modules/leaflet/dist/leaflet.css";
+</style>
+
+<script>
+import L from 'leaflet'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+
+export default {
+  name: 'issmap',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data () {
+    return {
+      zoom: 4.5,
+      url:'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}',
+      attribution:'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+      mapOptions: {
+        zoomSnap: 0.5,
+        scrollWheelZoom: false
+      },
+      icon: L.icon({
+        iconUrl: `/static/international_space_station.svg`,
+        iconSize: [64, 38],
+        iconAnchor: [32, 19]
+      })
+    }
+  },
+  props: ['position']
+}
+</script>
